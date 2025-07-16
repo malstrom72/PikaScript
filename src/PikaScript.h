fdc6b6fc52afc426f9ef5593ff3188c2a3bae9a4
Magnus Lidström <malstrom72@gmail.com>
2025-07-16 23:29:56 +0200

    Use tabs for license line

--- src/PikaScript.h
+++ src/PikaScript.h
@@ -9,7 +9,7 @@
 	
 	\page Copyright
 	
-	PikaScript is released under the "New Simplified BSD License". http://www.opensource.org/licenses/bsd-license.php
+	PikaScript is released under the BSD 2-Clause License. http://www.opensource.org/licenses/bsd-license.php
 	
 	Copyright (c) 2009-2019, NuEdge Development / Magnus Lidstroem
 	All rights reserved.
@@ -23,8 +23,6 @@
 	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
 	disclaimer in the documentation and/or other materials provided with the distribution. 
 	
-	Neither the name of the NuEdge Development nor the names of its contributors may be used to endorse or promote
-	products derived from this software without specific prior written permission.
 	
 	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
 	INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
