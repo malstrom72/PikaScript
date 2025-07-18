40915d52ef1610de2a64476f349a50c96754ed51
Magnus Lidström <malstrom72@gmail.com>
2025-07-18 13:20:08 +0200

    Add runnable PPEG doc example

--- build.cmd
+++ build.cmd
@@ -11,6 +11,9 @@ POPD
 tools\PikaCmd\SourceDistribution\PikaCmd tests\ppegTest.pika
 IF ERRORLEVEL 1 GOTO error
 
+tools\PikaCmd\SourceDistribution\PikaCmd examples\ppegDocExample.pika
+IF ERRORLEVEL 1 GOTO error
+
 PUSHD tools\PikaCmd\SourceDistribution
 DEL /Q PikaCmd.exe 2>NUL
 SET CPP_TARGET=release
@@ -18,6 +21,9 @@ CALL BuildPikaCmd.cmd
 IF ERRORLEVEL 1 GOTO error
 POPD
 tools\PikaCmd\SourceDistribution\PikaCmd tests\ppegTest.pika
+IF ERRORLEVEL 1 GOTO error
+
+tools\PikaCmd\SourceDistribution\PikaCmd examples\ppegDocExample.pika
 IF ERRORLEVEL 1 GOTO error
 
 EXIT /b 0
