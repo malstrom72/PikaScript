78c630972ae60e0435172cf6ebe515a91fd7726f
Magnus Lidström <malstrom72@gmail.com>
2025-07-19 20:46:19 +0200

    Rename UpdateDox scripts and improve usability

--- tools/UpdateDox.cmd
+++
@@ -1,8 +0,0 @@
-CD /D "%~dp0\.."
-tools\PikaCmd\build\Windows\MSVC10\Release\Win32\PikaCmd.exe tools\ExportHelp.pika
-tools\PikaCmd\build\Windows\MSVC10\Release\Win32\PikaCmd.exe tools\UpdateHtmlDox.pika
-doxygen docs\PikaScriptDoxyfile
-CD docs\latex
-pdflatex refman.tex
-CD ..
-move latex\refman.pdf PikaScript.pdf
