2236f5db5a0af17863b6f64231416c4d8ae94705
Magnus Lidström <malstrom72@gmail.com>
2025-07-25 00:23:53 +0200

    fix indentation

--- tools/PikaCmd/DownloadAndInstallPika.sh
+++ tools/PikaCmd/DownloadAndInstallPika.sh
@@ -3,24 +3,26 @@ set -e -o pipefail -u
 cd "$(dirname "$0")"
 
 if [ -z "${TMPDIR:-}" ]; then
-        TMPDIR="/tmp/"
+	TMPDIR="/tmp/"
 fi
 
 cd "${TMPDIR}"
-ARCHIVE="tar.gz"
+echo "Downloading, please wait..."
+ARCH="tar"
 if ! curl -fL https://github.com/malstrom72/PikaScript/releases/latest/download/PikaCmdSourceDistribution.tar.gz -o PikaCmdSourceDistribution.tar.gz; then
-        ARCHIVE="zip"
-        curl -fL https://github.com/malstrom72/PikaScript/releases/latest/download/PikaCmdSourceDistribution.zip -o PikaCmdSourceDistribution.zip
+	ARCH="zip"
+	curl -fL https://github.com/malstrom72/PikaScript/releases/latest/download/PikaCmdSourceDistribution.zip -o PikaCmdSourceDistribution.zip
 fi
 mkdir -p "PikaCmd"
 cd "PikaCmd"
 rm -rf "SourceDistribution" || true
-if [ "$ARCHIVE" = "tar.gz" ]; then
-        tar -xzf "../PikaCmdSourceDistribution.tar.gz"
-        rm -f "../PikaCmdSourceDistribution.tar.gz"
+echo "Extracting..."
+if [ "$ARCH" = "tar" ]; then
+	tar -xzf "../PikaCmdSourceDistribution.tar.gz"
+	rm -f "../PikaCmdSourceDistribution.tar.gz"
 else
-        unzip -q "../PikaCmdSourceDistribution.zip"
-        rm -f "../PikaCmdSourceDistribution.zip"
+	unzip -q "../PikaCmdSourceDistribution.zip"
+	rm -f "../PikaCmdSourceDistribution.zip"
 fi
 cd "SourceDistribution"
 bash BuildPikaCmd.sh
