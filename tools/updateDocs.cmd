ea61ee85b67d533f22e289dc2ef2d44bea194180
Magnus Lidström <malstrom72@gmail.com>
2025-07-27 17:15:59 +0200

    updateDocs: warn when doxygen missing

--- tools/updateDocs.cmd
+++ tools/updateDocs.cmd
@@ -1,8 +1,13 @@
 CD /D "%~dp0\.."
 output\PikaCmd.exe tools\ExportHelp.pika
 output\PikaCmd.exe tools\UpdateHtmlDox.pika
-doxygen docs\PikaScriptDoxyfile
-CD docs\latex
-pdflatex refman.tex
-CD ..
-move latex\refman.pdf PikaScript.pdf
+where doxygen >nul 2>nul
+IF %ERRORLEVEL% EQU 0 (
+    doxygen docs\PikaScriptDoxyfile
+    CD docs\latex
+    pdflatex refman.tex
+    CD ..
+    move latex\refman.pdf PikaScript.pdf
+) ELSE (
+    echo doxygen not found, skipping PDF build 1>&2
+)
