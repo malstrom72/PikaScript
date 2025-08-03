94a142253d5d3cc7e01582212272f3e9620f8115
Magnus Lidström <malstrom72@gmail.com>
2025-08-04 01:05:15 +0200

    Export fuzz build variables

--- tools/build_pikacmd_fuzz.sh
+++ tools/build_pikacmd_fuzz.sh
@@ -4,9 +4,11 @@ cd "$(dirname "$0")"/..
 
 mkdir -p output
 
-CPP_COMPILER="${CPP_COMPILER:-clang++}" \
-CPP_OPTIONS="-std=c++14 -fsanitize=fuzzer,address" \
+CPP_COMPILER="${CPP_COMPILER:-clang++}"
+CPP_OPTIONS="-std=c++14 -fsanitize=fuzzer,address"
+export CPP_COMPILER CPP_OPTIONS
+
 bash tools/PikaCmd/SourceDistribution/BuildCpp.sh debug native output/PikaCmdFuzz \
-		-DLIBFUZZ -DPLATFORM_STRING=UNIX -I src \
-		tools/PikaCmd/PikaCmd.cpp tools/PikaCmd/BuiltIns.cpp src/PikaScript.cpp src/QStrings.cpp
+                -DLIBFUZZ -DPLATFORM_STRING=UNIX -I src \
+                tools/PikaCmd/PikaCmd.cpp tools/PikaCmd/BuiltIns.cpp src/PikaScript.cpp src/QStrings.cpp
 
